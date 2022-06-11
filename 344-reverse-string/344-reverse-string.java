4e0deae39670134abfa58804d29d7e2ebe14c667
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-06-11 01:10:31 -0400

    Time: 1 ms (98.81%), Space: 54.3 MB (57.66%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,21 +1,17 @@
 class Solution {
     public void reverseString(char[] s) {
-        //1. We start with the base case. If the length of the string is less than or equal to 1, we return.
-        if (s == null || s.length <= 1) {
-            return;
-        }
-        //2. We swap the first and last characters.
-        reverse(s, 0, s.length-1);  
+        //1. We initialize two pointers, left and right, to point to the first and last characters of the string, respectively.
+        int left = 0;
+        int right = s.length - 1;
         
-    }
-    //3. We call the function recursively with the start index incremented by 1 and the end index decremented by 1.
-    public void reverse(char[] str, int start, int end) {
-        if (start >= end) {
-            return;
+        //We swap the characters at the two pointers until left is less than right.
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            //3. We decrement right and increment left until they meet.
+            left++;
+            right--;
         }
-        char ch = str[start];
-        str[start] = str[end];
-        str[end] = ch;
-        reverse(str, start +1, end-1);
     }
 }
