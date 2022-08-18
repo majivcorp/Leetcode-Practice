79ef0658c5ba969abbd43fbc761268b8c36d55a6
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-08-18 07:39:07 -0400

    Time: 1 ms (10.96%), Space: 41.4 MB (73.28%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -13,19 +13,33 @@
  *     }
  * }
  */
-//Approach 2: Recursive
+//Approach 1: Iterative
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        //1. If the root is null, return null.
-        if (root == null) {
+        if (root == null) 
+        {
             return null;
         }
-    
-        TreeNode right = invertTree(root.right);////2. Recursively invert the left subtree.
-        TreeNode left = invertTree(root.left); //3. Recursively invert the right subtree.
-        //4. Swap the left and right subtrees.
-        root.left = right;
-        root.right = left;
+        //1. We create a queue and add the root node to it.
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        
+        queue.add(root);
+        //2. We loop while the queue is not empty.
+        while (!queue.isEmpty()) {
+            //3. We poll the queue and store the current node in a temporary variable.
+            TreeNode current = queue.poll();
+            //4. We swap the left and right child nodes.
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            //5. We add the left and right child nodes to the queue if they are not null.
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
         return root;
     }
 }
